4b72159b8f45fb41ac0847c8a3095e30f5e23fa2
Virajj28 <viraj28j@gmail.com>
2022-02-02 16:51:17 +0530

    DataContextProvider resolved

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import App from './App';
+import DataContextProvider from './Context';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <DataContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </DataContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
